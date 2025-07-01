bb36431d5f5e1e81889eb18116b3cf374b84ed21
Aye Lecman <ayelenlecman@gmail.com>
2025-07-01 01:37:00 +0000

    creacion de la carta y funciones

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.onload = function () {
+  document.querySelector(".card-title").innerHTML = Math.floor(Math.random() * 13) + 1
+  document.querySelectorAll(".suit").forEach(elem => {
+    elem.innerHTML = suitRandom;
+  });
+}
+
+let suit = ["♦", "♠", "♥", "♣"]
+
+let suitRandom = suit[Math.floor(Math.random() * suit.length)]
